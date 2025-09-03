450ecc799e2f58ab0691b997ddc9f7e89797127d
Its-Sanskar <sanskarbt2@gmail.com>
2025-09-03 23:06:46 +0530

    add more code

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const express = require("express");
+// const express = require("express");
+import express from "express";
+import cors from "cors";
+import "dotenv/config.js";
+
 const app = express();
-require("dotenv").config();
-const port = 4000;
+app.use(express.static("public"));
+
+app.use(cors({ origin: "http://127.0.0.1:5500" }));
+const devData = (name) => {
+  return { name: name, age: 21, city: "Chhindwara" };
+};
 
 app.get("/", (req, res) => {
   res.send("hello World , <h1>this is Home page</h2>");
 });
-app.get("/about", (req, res) => {
-  res.send("this is aboutPage");
+app.get("/about/:name", (req, res) => {
+  res.json(devData(req.params.name));
 });
+app.get("/blog", (req, res) => {
+  res.send(
+    `<h1>name is ${req.query.name}</h1> <br> <h2>age is ${req.query.age}`
+  );
+});
+
 app.listen(process.env.PORT, () => {
   console.log("server is live");
 });
